ede0a0e30e45eb625d728acf029c8a8a37d749bd
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2024-10-11 09:19:58 +0200

    [NEW & UPDATE] Create the file constants.tsx and ad the constants to control the screen dimensions.

---
+++ src/constants/constants.tsx
@@ -0,0 +1,5 @@
+import { Dimensions } from "react-native";
+// controlar las dimensiones de pantalla, el espaciado entre tarjetas y el tamaño de los elementos ajustándolos inicialmente al 75%.
+const {width, height} = Dimensions.get('window');
+export const SPACING = 10;
+export const ITEM_SIZE = width * 0.75;
