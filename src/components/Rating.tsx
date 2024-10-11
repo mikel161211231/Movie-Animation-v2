d968aeaae5eb80d823eb9043afa3377b444b89c0
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2024-10-11 09:27:21 +0200

    [NEW & UPDATE] Create a new file rating.tsx for the rating components with its styled component

---
+++ src/components/Rating.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components/native'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+
+const Container = styled.View`
+    flex-direction: row;
+    margin-vertical: 4px;
+    align-items: center: center;
+    justify-content: center;`;
+
+
+export default function Rating({rating}){
+    
+    const totalOfFullStars = Math.floor(rating/2);
+    const starsOutLineArray = Array(5 - totalOfFullStars).fill('star-outline');
+    const fullStarArray = Array(totalOfFullStars).fill('star');
+    const ratingStars = [...fullStarArray, ...starsOutLineArray];
+    return(
+        <Container>
+            {ratingStars.map((icon, index) => {
+                return <Icon key={index} name={icon} size={16} color="gray" />;
+            })}
+        </Container>
+    );
+}
