f6f0eb0247f564efceb3186c7c7e8eb0030a0017
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2024-10-12 22:44:38 +0200

    [UPDATE] install the necessary library and add the new constants.

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -3,7 +3,10 @@ import { Dimensions } from "react-native";
 
 // controlar las dimensiones de pantalla, el espaciado entre tarjetas y el tamaño de los elementos ajustándolos inicialmente al 75%.
 const {width, height} = Dimensions.get('window');
+
+export const TOP = height * 0.20;
 export const SPACING = 10;
 export const ITEM_SIZE = width * 0.75;
-
-export const SPACER_ITEM_SIZE = (width -ITEM_SIZE)/2
+export const SPACER_ITEM_SIZE = (width -ITEM_SIZE)/2;
+export const BACKDROP_HEIGHT = height * 0.65;
+export const WIDTH = width;
