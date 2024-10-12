594fc2630ad455be31fabf32c5ee9ff41c6f0b0c
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2024-10-12 22:39:14 +0200

    [UPDATE] Add the advanced animation part III

--- App.tsx
+++ App.tsx
@@ -27,6 +27,18 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [allMmovies, setAllMovies] = useState([])
+  const data = movies;
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getMovies();
+      const leftSpacer = {key: 'left-spacer'};
+      const rightSpacer = {key: 'right-spacer'};
+      setAllMovies([leftSpacer, ...data, rightSpacer]);
+    }
+    fetchData();
+  }, []);
+  
   const scrollX = useRef(new Animated.Value(0)).current;
   
   return (
@@ -36,7 +48,7 @@ function App(): React.JSX.Element {
       <StatusBar />
       <Animated.FlatList
             showsHorizontalScrollIndicator={false}
-            data={movies} 
+            data={allMmovies} 
             keyExtractor={item => item.key}
             horizontal
             snapToInterval={CONSTANTS.ITEM_SIZE}
@@ -51,15 +63,19 @@ function App(): React.JSX.Element {
             scrollEventThrottle={16}
             renderItem={({item, index}) =>{
               const inputRange = [
+                (index -2)* CONSTANTS.ITEM_SIZE,
                 (index -1)* CONSTANTS.ITEM_SIZE,
-                index * CONSTANTS.ITEM_SIZE,
-                (index +1)* CONSTANTS.ITEM_SIZE
+                index * CONSTANTS.ITEM_SIZE
               ]
 
               const translateY = scrollX.interpolate({
                 inputRange,
                 outputRange: [0, -50, 0]
               })
+
+              if (!item.originalTitle) {
+                return <DummyContainer />
+              }
 
               return (
                 <PosterContainer>
@@ -102,24 +118,31 @@ const styles = StyleSheet.create({
 // Definir los STYLED COMPONENTS
 const Container = styled.View`
   flex: 1;`;
+
 const PosterContainer = styled.View`
   width: ${CONSTANTS.ITEM_SIZE}px;`;
+
 const Poster = styled.View`
   margin-horizontal: ${CONSTANTS.SPACING}px;
   padding: ${CONSTANTS.SPACING*2}px;
   align-items: center;
   background-color: #FFFFFF;
   border-radius: 10px;`;
+
 const PosterImage = styled.Image`
   width: 100%;
   height: ${CONSTANTS.ITEM_SIZE *1.2}px;
   resize-mode: cover;
   border-radius: 10px;
   margin: 0 0 10px 0;`;
+
 const PosterTitle = styled.Text`
   font-size: 18px;`;
+
 const PosterDescription = styled.Text`
   font-size: 12px;`;
   
+const DummyContainer = styled.View`
+  width: ${CONSTANTS.SPACER_ITEM_SIZE}px`;
 
 export default App;
--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,5 +1,9 @@
 import { Dimensions } from "react-native";
+
+
 // controlar las dimensiones de pantalla, el espaciado entre tarjetas y el tamaño de los elementos ajustándolos inicialmente al 75%.
 const {width, height} = Dimensions.get('window');
 export const SPACING = 10;
 export const ITEM_SIZE = width * 0.75;
+
+export const SPACER_ITEM_SIZE = (width -ITEM_SIZE)/2
